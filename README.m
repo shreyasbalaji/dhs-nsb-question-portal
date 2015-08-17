e81e18e9e8a2e02239338dbb4f3a7c8e64f0ef54
champinthere <shreyasb99@gmail.com>
2015-08-17 13:36:35 -0500

    Update and rename README.rdoc to README.m

---
+++ README.m
@@ -0,0 +1,8 @@
+== Dulles Science Bowl Question Portal
+
+This application powers the question portal Dulles Science Bowl
+uses to manage user-contributed questions. It runs on the [Ruby on Rails
+Platform](http://rubyonrails.org) and uses [Devise](https://github.com/plataformatec/devise) for authentication.
+
+It is currently configured for Ruby 2 and is set up for Heroku deployment.
+
